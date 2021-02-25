652009d15ee1d43505e116f33884765f61ec6c2a
timofeyh <thartano@uwo.com>
2021-02-25 15:28:33 -0500

    Added details to spawn

--- Player/Assets/Scripts/Move.cs
+++ Player/Assets/Scripts/Move.cs
@@ -3,60 +3,76 @@
 public class Move : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform playerCamera;
+    public Camera playerCamera;
+    public Camera perspectiveCamera;
     public CharacterController character;
     public Transform head;
-    public float sensitivity = 400f;
+    public static float sensitivity = 400f;
     private float xRotate = 0f;
     public bool mouseLock = true;
-    public float speed = 0.2f;
-    public float gravity = -20f;
+    public static float speed = 0.1f;
+    public static float gravity = -20f;
     public float yVelocity = 0f;
+    public static float jump = 10f;
     public bool grounded;
     public Transform feet;
     public LayerMask ground;
-    public bool sprintToggle = false;
+    public static bool sprintToggle = false;
     public KeyCode togglePerspective = KeyCode.Space;
+    public KeyCode toggleSprint = KeyCode.LeftShift;
 
 
     void Start()
     {
+        playerCamera.enabled = true;
+        perspectiveCamera.enabled = false;
         if (mouseLock) {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
     }
 
-    private void FixedUpdate()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         UpdateMouseMove();
-        UpdatePlayerWalk();
 
 
         if (Input.GetKeyDown(togglePerspective)) {
-            playerCamera.transform.localPosition = new Vector3(0, 0, -5);
+            playerCamera.enabled = false;
+            perspectiveCamera.enabled = true;
         }
         if (Input.GetKeyUp(togglePerspective)) {
-            playerCamera.transform.localPosition = new Vector3(0,0,0);
+            playerCamera.enabled = true;
+            perspectiveCamera.enabled = false;
         }
 
-        if (sprintToggle && (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))) {
+        if (sprintToggle && (Input.GetKeyUp(KeyCode.W))) {
             sprintToggle = false;
-            speed /= 2;
         }
+        if (Input.GetKey(toggleSprint)) {
+            sprintToggle = true;
+        }
+
+        if (sprintToggle) { 
+            UpdatePlayerWalk(speed * 2);
+        } else {
+
+        }  UpdatePlayerWalk(speed);
 
         yVelocity += gravity * Time.deltaTime;
         if (checkGrounded())
         {
             yVelocity = 0;
         }
-        
+
+
+        if (Input.GetKeyDown(KeyCode.F) && grounded)
+        {
+            yVelocity = jump;
+            grounded = false;
+        }
+
         character.Move(Vector3.up * yVelocity * Time.deltaTime);
 
     }
@@ -83,9 +99,9 @@ public class Move : MonoBehaviour
         return grounded;
     }
 
-    void UpdatePlayerWalk() {
-        Vector3 move = this.transform.forward * Input.GetAxis("Vertical") +this.transform.right * Input.GetAxis("Horizontal");
-
-        character.Move(move*speed);
+    void UpdatePlayerWalk(float spd) {
+        Vector3 move;
+        move = this.transform.forward * Input.GetAxis("Vertical") + this.transform.right * Input.GetAxis("Horizontal");
+        character.Move(move * spd);
     }
 }
---
+++ Player/Assets/Walk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Move.sprintToggle)
+            {
+                anim.SetTrigger("a_Running");
+                anim.ResetTrigger("a_Idle");
+                anim.ResetTrigger("a_Walking");
+            }
+            else {
+                anim.SetTrigger("a_Walking");
+                anim.ResetTrigger("a_Idle");
+                anim.ResetTrigger("a_Running");
+            }
+        }
+        else {
+            anim.SetTrigger("a_Idle");
+            anim.ResetTrigger("a_Running");
+            anim.ResetTrigger("a_Walking");
+        }
+    }
+}
