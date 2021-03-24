1ff118c207ed1e141ea9ca3051ee036d98b9f987
zbennet <zbennet2@uwo.ca>
2021-03-24 02:09:59 -0400

    Scene Transitions

---
+++ Player/Assets/_Scripts/SwitchScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SwitchScene : MonoBehaviour
+{
+ void OnTriggerEnter(Collider other)//when player triggers the box
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//move to the current scene +1 in the build settings
+    }
+}
